acc3bbbff04ea9623a1fa75542a0fca1443e3344
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-25 17:21:25 +0600

    final update

--- js/event.js
+++ js/event.js
@@ -1,19 +1,13 @@
-document.getElementById('bar').addEventListener('click', function (){
-                       
-    document.getElementById('bar').classList.add('hidden');
-    document.getElementById('cross').classList.remove('hidden');
-    
-    document.getElementById('menuDropdown').classList.remove('hidden');
+document.getElementById("bar").addEventListener("click", function () {
+  document.getElementById("bar").classList.add("hidden");
+  document.getElementById("cross").classList.remove("hidden");
 
+  document.getElementById("menuDropdown").classList.remove("hidden");
+});
 
- })
+document.getElementById("cross").addEventListener("click", function () {
+  document.getElementById("cross").classList.add("hidden");
+  document.getElementById("bar").classList.remove("hidden");
 
- document.getElementById('cross').addEventListener('click', function (){
-    
-    document.getElementById('cross').classList.add('hidden');
-    document.getElementById('bar').classList.remove('hidden');
-    
-    document.getElementById('menuDropdown').classList.add('hidden');
-
-
- })
+  document.getElementById("menuDropdown").classList.add("hidden");
+});
